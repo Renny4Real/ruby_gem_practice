9b4a3aa78faf084fbebeedf64e9b157d0529e320
Renny Fadoju <rennyvbirds@yahoo.com>
2018-07-16 14:46:36 +0100

    add code to word wrap ruby file

--- lib/word_wrapping.rb
+++ lib/word_wrapping.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
 require "word_wrapping/version"
 
 module WordWrapping
-  # Your code goes here...
+  def wrap(string, max_col_length)
+    string.length <= max_col_length ? string : column_break(string, max_col_length)
+  end
+
+  private
+
+  def column_break(string, max_col_length)
+    column_break = string[0...max_col_length].rindex(' ') || max_col_length
+    string[0...column_break].strip + "\n" \
+    + wrap(string[column_break..-1].strip, max_col_length)
+  end
+  
 end
