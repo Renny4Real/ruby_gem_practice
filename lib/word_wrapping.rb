f26012b2d5d5bb59e359afa1e91b1ae1978cef25
Renny Fadoju <rennyvbirds@yahoo.com>
2018-07-16 15:38:13 +0100

    Add more code and check tests pass

--- lib/word_wrapping.rb
+++ lib/word_wrapping.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
-require "word_wrapping/version"
+
+require 'word_wrapping/version'
 
 module WordWrapping
   class Word
-
     def self.wrap(string, max_col_length)
       string.length <= max_col_length ? string : column_break(string, max_col_length)
     end
 
-    private
+    private_class_method
 
-    def column_break(string, max_col_length)
+    def self.column_break(string, max_col_length)
       column_break = string[0...max_col_length].rindex(' ') || max_col_length
       string[0...column_break].strip + "\n" \
       + wrap(string[column_break..-1].strip, max_col_length)
     end
-
   end
 end
--- spec/word_wrapping_spec.rb
+++ spec/word_wrapping_spec.rb
@@ -1,12 +1,63 @@
+# frozen_string_literal: true
+
 RSpec.describe WordWrapping do
-  it "has a version number" do
+  it 'has a version number' do
     expect(WordWrapping::VERSION).not_to be nil
   end
 
-  describe WordWrapping::Word do
-    it 'returns an empty string when called with nothing' do
-      expect(WordWrapping::Word.wrap('', 1)).to eq('')
+  context WordWrapping::Word do
+    describe 'when called with nothing' do
+      it 'returns an empty string when called with nothing' do
+        expect(WordWrapping::Word.wrap('', 1)).to eq('')
+      end
+    end
+
+    describe 'when called with a word shorter than the max column length' do
+      it 'returns the complete word' do
+        expect(WordWrapping::Word.wrap('abcd', 5)).to eq('abcd')
+      end
+    end
+  
+    describe 'when max column length falls between two words on a space' do
+      it 'splits the two words on the space' do
+        expect(WordWrapping::Word.wrap('abcd efgh', 5)).to eq("abcd\nefgh")
+      end
+    end
+  
+    describe 'when max column length falls on the space between multiple words' do
+      it 'splits these words on the space' do
+        expect(WordWrapping::Word.wrap('abcd efgh ijkl', 5)).to eq("abcd\nefgh\nijkl")
+      end
+    end
+  
+    describe 'when many words are on a line within the max column length' do
+      it 'keeps as many words on the line as possible' do
+        expect(WordWrapping::Word.wrap('abcd efgh ijkl mnop', 10)).to eq("abcd efgh\nijkl mnop")
+      end
+    end
+  
+    describe 'when the max column length falls within a word' do
+      it 'splits the word at the max column length' do
+        expect(WordWrapping::Word.wrap('abcdefgh', 6)).to eq("abcdef\ngh")
+      end
+    end
+  
+    describe 'when max column length is after a space and the remaining characters also fall within the max column length' do
+      it 'splits early on the word at that space and strips the whitespace before adding the new line' do
+        expect(WordWrapping::Word.wrap('abcd efgh', 7)).to eq("abcd\nefgh")
+      end
+    end
+  
+    describe 'when max column length is immediatley before a space and remaining characters also fall within the max column length' do
+      it 'splits the word at that space and strips the whitespace before adding the new line' do
+        expect(WordWrapping::Word.wrap('abcd efgh', 4)).to eq("abcd\nefgh")
+      end
+    end
+  
+    describe 'when max column length falls on a really long word' do
+      it 'splits the long word at every instance that the max column point is reached' do
+        expect(WordWrapping::Word.wrap('abcdefghijklmnopqrstuvwxyz', 2)).to eq("ab\ncd\nef\ngh\nij\nkl\nmn\nop\nqr\nst\nuv\nwx\nyz")
+      end
     end
   end
-
 end
